c87ab267e75d04d9d4bfe8f712f3f36ecbe6954c
Ahmed <ahmedabozaid.dev@gmail.com>
2024-10-25 16:41:58 +0300

    updates the store to use local storage

--- src/stores/tasks/tasksSlice.js
+++ src/stores/tasks/tasksSlice.js
@@ -1,16 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const getInitialState = () => {
+  const storedTasks = localStorage.getItem("tasks");
+  if (storedTasks) {
+    return JSON.parse(storedTasks);
+  }
+  return [];
+};
+
 const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
-    tasks: [],
+    tasks: getInitialState(),
   },
   reducers: {
     addTask: (state, action) => {
       state.tasks.push(action.payload);
+      localStorage.setItem("tasks", JSON.stringify(state.tasks));
     },
-    editTask: (state, action) => {},
+    editTask: (state, action) => {
+      const { id, task } = action.payload;
+      const index = state.tasks.findIndex((task) => task.id === id);
+      if (index !== -1) {
+        state.tasks[index] = task;
+        localStorage.setItem("tasks", JSON.stringify(state.tasks));
+      }
+    },
     deleteTask: (state, action) => {
       state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+      localStorage.setItem("tasks", JSON.stringify(state.tasks));
     },
   },
 });
