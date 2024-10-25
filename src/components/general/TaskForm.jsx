87179ca3d47882e05d390ac071c53333600cf787
Ahmed <ahmedabozaid.dev@gmail.com>
2024-10-25 15:25:43 +0300

    adds add task form layout

--- src/components/general/TaskForm.jsx
+++ src/components/general/TaskForm.jsx
@@ -9,6 +9,8 @@ import {
   Input,
   Button,
   useDisclosure,
+  Select,
+  SelectItem,
 } from "@nextui-org/react";
 const TaskForm = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -27,27 +29,51 @@ const TaskForm = () => {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalHeader className="flex flex-col gap-5">
+                Create a new Task
+              </ModalHeader>
               <ModalBody>
                 <Input
                   autoFocus
-                  label="Email"
-                  placeholder="Enter your email"
+                  label="Title"
+                  placeholder="Enter the task name"
                   variant="bordered"
                 />
                 <Input
-                  label="Password"
-                  placeholder="Enter your password"
+                  label="Description"
+                  placeholder="Enter the task description"
                   type="password"
                   variant="bordered"
                 />
+                <Select
+                  placeholder="Select Priority"
+                  variant="bordered"
+                  classNames={{ trigger: "py-6" }}
+                >
+                  <SelectItem key="low">Low</SelectItem>
+                  <SelectItem key="medium">Medium</SelectItem>
+                  <SelectItem key="high">High</SelectItem>
+                </Select>
+                <Select
+                  placeholder="Task state"
+                  variant="bordered"
+                  classNames={{ trigger: "py-6" }}
+                >
+                  <SelectItem key="todo">To do</SelectItem>
+                  <SelectItem key="doing">Doing</SelectItem>
+                  <SelectItem key="done">Done</SelectItem>
+                </Select>
               </ModalBody>
-              <ModalFooter>
+              <ModalFooter className="flex justify-between">
                 <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
+                  Cancel
                 </Button>
-                <Button color="primary" onPress={onClose}>
-                  Sign in
+                <Button
+                  className="bg-primary-purple"
+                  color="primary"
+                  onPress={onClose}
+                >
+                  Add Task
                 </Button>
               </ModalFooter>
             </>
