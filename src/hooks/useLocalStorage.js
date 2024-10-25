c530fc3282e043376ff253820192cbf2569745ad
Ahmed <ahmedabozaid.dev@gmail.com>
2024-10-25 16:37:39 +0300

    adds useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const getStoredValue = () => {
+    const storedValue = localStorage.getItem(key);
+    if (storedValue) {
+      return JSON.parse(storedValue);
+    }
+    return initialValue;
+  };
+
+  const [value, setValue] = useState(getStoredValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  const removeValue = () => {
+    localStorage.removeItem(key);
+    setValue(initialValue);
+  };
+
+  return [value, setValue, removeValue];
+};
+
+export default useLocalStorage;
